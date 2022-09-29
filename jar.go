9a43d8f18dfcb076ac76fab40fce5c726c4009bc
Leoo0x1 <leonardomarchi14@gmail.com>
2022-09-29 10:52:41 +0200

    removed get cookies

--- jar.go
+++ jar.go
@@ -6,28 +6,7 @@ package http
 
 import (
 	"net/url"
-	"time"
 )
-
-type entry struct {
-	Name       string
-	Value      string
-	Domain     string
-	Path       string
-	SameSite   string
-	Secure     bool
-	HttpOnly   bool
-	Persistent bool
-	HostOnly   bool
-	Expires    time.Time
-	Creation   time.Time
-	LastAccess time.Time
-
-	// seqNum is a sequence number so that Cookies returns cookies in a
-	// deterministic order, even for cookies that have equal Path length and
-	// equal Creation time. This simplifies testing.
-	seqNum uint64
-}
 
 // A CookieJar manages storage and use of cookies in HTTP requests.
 //
@@ -45,6 +24,4 @@ type CookieJar interface {
 	// It is up to the implementation to honor the standard cookie use
 	// restrictions such as in RFC 6265.
 	Cookies(u *url.URL) []*Cookie
-
-	GetCookies() map[string]map[string]entry
 }
